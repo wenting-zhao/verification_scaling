e73128cdcfae9dc89a2e8de23c38dd912af1da22
Wenting Zhao <wentingzhao@outlook.com>
2025-04-26 01:41:58 -0400

    added execution code

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def parse_args():
     parser.add_argument("--code_prompt_format", type=str, default="std", help="Prompt format for code generation")
     parser.add_argument("--test_prompt_format", type=str, default="instruction_only", help="Prompt format for test generation")
     parser.add_argument("--output_dataset_name", type=str, default="wentingzhao/livecodebench_generated", help="Output dataset name")
-
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
@@ -38,8 +38,30 @@ def main():
     )
     dataset = dataset.add_column(name="generated_code", column=generated_code)
     test_inputs, test_outputs = generate_tests(problems, args.test_prompt_format, args.verification_model)
-    dataset = dataset.add_column(name="generated_tests", column=[{"input": input, "output": output} for input, output in zip(test_inputs, test_outputs)])
+    verification_info = []
+    for test_input, test_output in zip(test_inputs, test_outputs):
+        verification_info.append({
+            "language": "python",
+            "test_cases": [
+                {
+                    "input": "\n".join(test_input),
+                    "output": "\n".join(test_output),
+                    "type": "stdin_stdout",
+                }
+            ],
+        })
+    dataset = dataset.add_column(name="verification_info", column=verification_info)
+    test_completions = []
+    reward_kwargs = dict()
+    reward_kwargs["verification_info"] = []
+    for example in dataset:
+        test_completions += [[{"content": one}] for one in example["generated_code"]]
+        reward_kwargs["verification_info"] += [example["verification_info"] for _ in example["generated_code"]]
+    rewards = code_reward(test_completions, num_parallel=256, **reward_kwargs)
+    rewards = [rewards[i:i+len(example["generated_code"])] for i in range(0, len(rewards), len(example["generated_code"]))]
+    dataset = dataset.add_column(name="rewards", column=rewards)
     dataset.push_to_hub(args.output_dataset_name, split=args.dataset_split)
+
 
 if __name__ == "__main__":
     main()
